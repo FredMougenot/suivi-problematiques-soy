bd0e0932252c73bbad37fe6a42e120e73e99c4d6
Expedition-Dept <fmougenot@soylutions.ca>
2026-07-06 20:06:00 -0400

    Rename VolumetricStudio to NeuralVortexBackground

--- src/features/auth/LoginPage.jsx
+++ src/features/auth/LoginPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Mail, Lock, Eye, EyeOff } from 'lucide-react';
 import { useAuthStore } from '../../store/useAuthStore';
-import VolumetricStudio from './NeuralVortexBackground';
+import NeuralVortexBackground from './NeuralVortexBackground';
 import './login.css';
 
 const REMEMBER_KEY = 'soy_remember_email';
@@ -47,7 +47,7 @@ export default function LoginPage() {
 
   return (
     <div className="lg-page">
-      <VolumetricStudio />
+      <NeuralVortexBackground />
       <div className="lg-scrim" />
 
       <div className="lg-card">
